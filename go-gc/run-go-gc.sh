eb007feb85796b2b6c8f7e2ee18b4e7dd90e3035
joey0320 <joonho0320@gmail.com>
2023-11-17 22:36:37 -0800

    vary GOGC instead of thread pinning stuff

--- go-gc/run-go-gc.sh
+++ go-gc/run-go-gc.sh
@@ -7,33 +7,46 @@ touch GO_GC_RESULTS.out
 echo "GO Garbage Collection Results" > GO_GC_RESULTS.out
 
 
-echo "Pin threads to GOMAXPROCS cores" >> GO_GC_RESULTS.out
+echo "Pin threads to GOMAXPROCS cores, GOGC=100" >> GO_GC_RESULTS.out
 echo "GOMAXPROCS 1" >> GO_GC_RESULTS.out
-GOMAXPROCS=1  taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
+GOMAXPROCS=1  GOGC=100 taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
 
 echo "GOMAXPROCS 2" >> GO_GC_RESULTS.out
-GOMAXPROCS=2  taskset -c 0,1 ./main.riscv >> GO_GC_RESULTS.out
+GOMAXPROCS=2  GOGC=100 taskset -c 0,1 ./main.riscv >> GO_GC_RESULTS.out
 
 echo "GOMAXPROCS 3" >> GO_GC_RESULTS.out
-GOMAXPROCS=3  taskset -c 0,1,2 ./main.riscv >> GO_GC_RESULTS.out
+GOMAXPROCS=3  GOGC=100 taskset -c 0,1,2 ./main.riscv >> GO_GC_RESULTS.out
 
 echo "GOMAXPROCS 4" >> GO_GC_RESULTS.out
-GOMAXPROCS=4  taskset -c 0,1,2,3 ./main.riscv >> GO_GC_RESULTS.out
+GOMAXPROCS=4  GOGC=100 taskset -c 0,1,2,3 ./main.riscv >> GO_GC_RESULTS.out
 
-
-
-echo "Pin threads to 1 cores" >> GO_GC_RESULTS.out
+echo "Pin threads to GOMAXPROCS cores, GOGC=10" >> GO_GC_RESULTS.out
 echo "GOMAXPROCS 1" >> GO_GC_RESULTS.out
-GOMAXPROCS=1  taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
+GOMAXPROCS=1  GOGC=10 taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
 
 echo "GOMAXPROCS 2" >> GO_GC_RESULTS.out
-GOMAXPROCS=2  taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
+GOMAXPROCS=2  GOGC=10 taskset -c 0,1 ./main.riscv >> GO_GC_RESULTS.out
 
 echo "GOMAXPROCS 3" >> GO_GC_RESULTS.out
-GOMAXPROCS=3  taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
+GOMAXPROCS=3  GOGC=10 taskset -c 0,1,2 ./main.riscv >> GO_GC_RESULTS.out
 
 echo "GOMAXPROCS 4" >> GO_GC_RESULTS.out
-GOMAXPROCS=4  taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
+GOMAXPROCS=4  GOGC=10 taskset -c 0,1,2,3 ./main.riscv >> GO_GC_RESULTS.out
+
+
+
+# echo "Pin threads to 1 cores" >> GO_GC_RESULTS.out
+# echo "GOMAXPROCS 1" >> GO_GC_RESULTS.out
+# GOMAXPROCS=1  taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
+
+# echo "GOMAXPROCS 2" >> GO_GC_RESULTS.out
+# GOMAXPROCS=2  taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
+
+# echo "GOMAXPROCS 3" >> GO_GC_RESULTS.out
+# GOMAXPROCS=3  taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
+
+# echo "GOMAXPROCS 4" >> GO_GC_RESULTS.out
+# GOMAXPROCS=4  taskset -c 0 ./main.riscv >> GO_GC_RESULTS.out
 
 
 poweroff -f
--- run-go-gc-x86.sh
+++ run-go-gc-x86.sh
@@ -4,11 +4,11 @@ set -x
 touch X86_GO_GC_RESULTS.out
 echo "GO Garbage Collection Results" > X86_GO_GC_RESULTS.out
 
-echo "Pin threads to 1 core"          >> X86_GO_GC_RESULTS.out
-GOMAXPROCS=1  taskset -c 0 ./main.x86 >> X86_GO_GC_RESULTS.out
-GOMAXPROCS=2  taskset -c 0 ./main.x86 >> X86_GO_GC_RESULTS.out
-GOMAXPROCS=3  taskset -c 0 ./main.x86 >> X86_GO_GC_RESULTS.out
-GOMAXPROCS=4  taskset -c 0 ./main.x86 >> X86_GO_GC_RESULTS.out
+# echo "Pin threads to 1 core"          >> X86_GO_GC_RESULTS.out
+# GOMAXPROCS=1  taskset -c 0 ./main.x86 >> X86_GO_GC_RESULTS.out
+# GOMAXPROCS=2  taskset -c 0 ./main.x86 >> X86_GO_GC_RESULTS.out
+# GOMAXPROCS=3  taskset -c 0 ./main.x86 >> X86_GO_GC_RESULTS.out
+# GOMAXPROCS=4  taskset -c 0 ./main.x86 >> X86_GO_GC_RESULTS.out
 
 echo "Pin threads to GOMAXPROCS cores"      >> X86_GO_GC_RESULTS.out
 GOMAXPROCS=1  taskset -c 0       ./main.x86 >> X86_GO_GC_RESULTS.out
@@ -16,11 +16,17 @@ GOMAXPROCS=2  taskset -c 0,1     ./main.x86 >> X86_GO_GC_RESULTS.out
 GOMAXPROCS=3  taskset -c 0,1,2   ./main.x86 >> X86_GO_GC_RESULTS.out
 GOMAXPROCS=4  taskset -c 0,1,2,3 ./main.x86 >> X86_GO_GC_RESULTS.out
 
-echo "Pin thread to separate NUMA domains"  >> X86_GO_GC_RESULTS.out
-CORES_PER_SOCKET=$(lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l)
-SOCKET_COUNT=$(lscpu -b -p=Socket | grep -v '^#' | sort -u | wc -l)
-echo "$CORES_PER_SOCKET cores per socket, $SOCKET_COUNT sockets"
-if [ $SOCKET_COUNT -ne 2 ]; then
-  exit 1
-fi
-GOMAXPROCS=2  taskset -c 0,$CORES_PER_SOCKET    ./main.x86 >> X86_GO_GC_RESULTS.out
+# echo "Pin thread to separate NUMA domains"  >> X86_GO_GC_RESULTS.out
+# CORES_PER_SOCKET=$(lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l)
+# SOCKET_COUNT=$(lscpu -b -p=Socket | grep -v '^#' | sort -u | wc -l)
+# echo "$CORES_PER_SOCKET cores per socket, $SOCKET_COUNT sockets"
+# if [ $SOCKET_COUNT -ne 2 ]; then
+# exit 1
+# fi
+# GOMAXPROCS=2  taskset -c 0,$CORES_PER_SOCKET    ./main.x86 >> X86_GO_GC_RESULTS.out
+
+echo "Pin threads to GOMAXPROCS cores GOGC=1"      >> X86_GO_GC_RESULTS.out
+GOMAXPROCS=1  GOGC=10 taskset -c 0       ./main.x86 >> X86_GO_GC_RESULTS.out
+GOMAXPROCS=2  GOGC=10 taskset -c 0,1     ./main.x86 >> X86_GO_GC_RESULTS.out
+GOMAXPROCS=3  GOGC=10 taskset -c 0,1,2   ./main.x86 >> X86_GO_GC_RESULTS.out
+GOMAXPROCS=4  GOGC=10 taskset -c 0,1,2,3 ./main.x86 >> X86_GO_GC_RESULTS.out
