3c9b58ee191268dfbc849dff29e2d2d40ca42028
joey0320 <joonho0320@gmail.com>
2023-11-17 19:32:02 -0800

    Add x86 numa domain stuff

--- run-go-gc-x86.sh
+++ run-go-gc-x86.sh
@@ -17,4 +17,10 @@ GOMAXPROCS=3  taskset -c 0,1,2   ./main.x86 >> X86_GO_GC_RESULTS.out
 GOMAXPROCS=4  taskset -c 0,1,2,3 ./main.x86 >> X86_GO_GC_RESULTS.out
 
 echo "Pin thread to separate NUMA domains"  >> X86_GO_GC_RESULTS.out
-GOMAXPROCS=2  taskset -c 0,36    ./main.x86 >> X86_GO_GC_RESULTS.out
+CORES_PER_SOCKET=$(lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l)
+SOCKET_COUNT=$(lscpu -b -p=Socket | grep -v '^#' | sort -u | wc -l)
+echo "$CORES_PER_SOCKET cores per socket, $SOCKET_COUNT sockets"
+if [ $SOCKET_COUNT -ne 2 ]; then
+  exit 1
+fi
+GOMAXPROCS=2  taskset -c 0,$CORES_PER_SOCKET    ./main.x86 >> X86_GO_GC_RESULTS.out
